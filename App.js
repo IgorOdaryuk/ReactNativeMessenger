f6861accae396abe31a77c2ee071e319da52ad9f
IgorOdaryuk <odaryukigor@gmail.com>
2024-04-29 00:47:59 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,99 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {Entypo} from '@expo/vector-icons';
+
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Vadim Savin",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.post}>
+        {}
+        <View style={styles.header}>
+          <Image 
+            source={{uri: post.User.image}} 
+            style={styles.profileImage} 
+          />
+          <View>
+            <Text style={styles.name}>{post.User.name}</Text>
+            <Text style={styles.subtitle}>{post.createdAt}</Text>
+          </View>
+          <Entypo 
+            name="dots-three-horizontal" 
+            size={18} 
+            color="gray" 
+            style={styles.icon}
+          />
+        </View>
+
+        {}
+        <Text style={styles.description}>{post.description}</Text>
+        <Image source={{ uri: post.image }} style={styles.image} />
+
+        {}
+        <View style={styles.footer}>
+
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  post: {
+    width: "100%",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "500,"
+  },
+  subtitle: {
+    color: "gray",
+  },
+  icon: {
+    marginLeft: "auto",
+  },
+  description: {
+    paddingHorizontal: 10,
+    lineHeight: 20,
+    letterSpacing: 0.3,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    marginTop: 10,
+  },
+});
